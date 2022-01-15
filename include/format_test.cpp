8b3e544fbad78f279eeff99cf5ae709e5130088a
beta-cyg <beta-Cyg@github.com>
2022-01-15 11:11:37 +0800

    problem: the result of thread_que.size() is wrong; cnm

--- include/format_test.cpp
+++ include/format_test.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    cout<<cyg::format("{} is old than {}","Mary","Tom")<<endl;
+    cout<<cyg::format("{} is old than {}.","Mary","Tom")<<endl;
 
     return 0;
 }
